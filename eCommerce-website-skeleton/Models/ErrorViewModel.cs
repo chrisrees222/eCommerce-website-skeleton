b5ff2830e5c9b9c8920aa79e5f52adaf560d364d
chrisrees222 <143870938+chrisrees222@users.noreply.github.com>
2024-06-05 20:34:12 +0100

    Add project files.

---
+++ eCommerce-website-skeleton/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eCommerce_website_skeleton.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
